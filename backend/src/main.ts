d7e0b3eadd1624cc91d180b1f649f0b7b65e656b
Laxmipriya-coder <sachitramishra70@gmail.com>
2026-01-11 16:39:42 +0530

    Updated: added new folder and modified code

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { ScrapeService } from './scrape/scrape.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
   const scrapeService = app.get(ScrapeService);
   // await scrapeService.scrapeNavigation(); -delete to work API work
 }
